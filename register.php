c61c2d51a03d6031df0abad32183bc176870e6f3
nraeder <nar28@njit.edu>
2017-05-03 22:03:54 -0400

    Update register.php

--- register.php
+++ register.php
@@ -5,23 +5,23 @@
 <body>
 
 <?php
-	include_once 'connectDatabase.php';
+	include_once 'database.php';
 	
 	//Let's make sure the correct data is received. 
-	if (!isset($_REQUEST['email']) || !isset($_REQUEST['password'])){
+	if (!isset($_REQUEST['Email']) || !isset($_REQUEST['Password'])){
 		header('HTTP/1.1 500 Internal Server Error');
 		exit("ERROR: There was an error writing to the database. Some required data was missing.<br><a href='index.php'>Go back to main page.</a>");
 	}
-	else if ($_REQUEST['email'] == null || $_REQUEST['password'] == null){
+	else if ($_REQUEST['Email'] == null || $_REQUEST['Password'] == null){
 		header('HTTP/1.1 500 Internal Server Error');
 		exit("ERROR: There was an error writing to the database. Some required data was blank.<br><a href='index.php'>Go back to main page.</a>");
 	}
 	
-	$useremail = $_REQUEST['email'];
-	$userpass  = $_REQUEST['password'];
+	$useremail = $_REQUEST['Email'];
+	$userpass  = $_REQUEST['Password'];
 
 	//Let's make sure the e-mail doesn't already exist.
-	$sql = 'SELECT * FROM accounts where email="'.$useremail.'"';
+	$sql = 'SELECT * FROM Account and Profile Info where email="'.$useremail.'"';
 	$results = runQuery($sql);
 	
 	//If the following line has results (the array length is more than 0), that means data/e-mail already exists.
@@ -31,7 +31,7 @@
 	}
 
 	//Let's add the data.
-	$sql = 'INSERT INTO accounts (`email`, `password`) VALUES ("'.$useremail.'", "'.$userpass.'")';
+	$sql = 'INSERT INTO Account and Profile Info (`email`, `password`) VALUES ("'.$useremail.'", "'.$userpass.'")';
 	$results = runQuery($sql);
 	
 	echo "User Registered.";
