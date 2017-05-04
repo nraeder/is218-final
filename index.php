0d7f5d23d7ded57c35697c9997430f66d25f84db
nraeder <nar28@njit.edu>
2017-05-04 00:27:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ div {
 	font-family: "Rockwell";
 	font-size: 20px;
 	color: white;
-	margin-left: auto;
-	margin-right: auto;
-	margin-top: 50px;
+	margin-left: 100px;
+	margin-right: 100px;
+	margin-top: 100px;
 	align-content: left;
 }
 	
